b9c5330f540ad221b9975331d5f88a57b848ce90
Rayane Souza <130245094+Rayane-Souza@users.noreply.github.com>
2024-09-03 15:18:16 -0300

    DecBase - commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  document.getElementById('menu-icon').addEventListener('click', function() {
+    const navList = document.querySelector('.navlist');
+    navList.classList.toggle('active');
+  });
+
